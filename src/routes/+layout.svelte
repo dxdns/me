d542035833d828e92e667f654d431dbc84e6f6b0
dxdns <37380674+dxdns@users.noreply.github.com>
2025-08-05 10:13:02 -0300

    first

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import favicon from "$lib/assets/favicon.ico"
+	import { FeflowProvider } from "@dxdns/feflow-svelte"
+
+	let { children } = $props()
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<FeflowProvider defaultThemeMode="dark">
+	{@render children?.()}
+</FeflowProvider>
+
+<style>
+	@font-face {
+		font-family: "Inter";
+		src: url("/fonts/Inter-Regular.woff2") format("woff2");
+		font-weight: 400;
+		font-style: normal;
+		font-display: swap;
+	}
+
+	@font-face {
+		font-family: "Inter";
+		src: url("/fonts/Inter-Bold.woff2") format("woff2");
+		font-weight: 700;
+		font-style: normal;
+		font-display: swap;
+	}
+
+	:global(body) {
+		font-family: "Inter", sans-serif;
+		line-height: 1.2;
+		overflow-x: hidden;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,70 @@
+<script lang="ts">
+	import { Avatar, Card, Link } from "@dxdns/feflow-svelte"
+	import profileImage from "$lib/assets/profile.jpeg"
+
+	let { data } = $props()
+</script>
+
+<svelte:head>
+	<title>me | {data.title}</title>
+</svelte:head>
+
+<div
+	style="
+    line-height: 1.5;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+	height: 100vh;
+    "
+>
+	<Card
+		animatedBorder
+		style="
+		display: flex;
+		align-items: center;
+		flex-direction: column;
+		padding: 3rem;
+		border-radius: 16px;
+		text-align: center;
+		"
+	>
+		<div class="rounded-full gradient-border">
+			<Avatar
+				alt="profile"
+				textFallback="Diógenes Rodrigues"
+				src={profileImage}
+				height="175px"
+				width="175px"
+			/>
+		</div>
+		<br />
+		<h2>Diógenes Rodrigues</h2>
+		<Link
+			hoverUnderline="right"
+			href="https://github.com/dxdns"
+			target="_blank"
+		>
+			<small>@dxdns</small>
+		</Link>
+	</Card>
+</div>
+
+<style>
+	.gradient-border {
+		--angle: 0deg;
+		--primary: #39ff14;
+		--secondary: #00ffff;
+		border: 4px solid transparent;
+		background: conic-gradient(
+			from var(--angle),
+			var(--secondary) 80%,
+			var(--primary) 88%,
+			var(--primary) 92%,
+			var(--secondary) 100%
+		);
+		background-origin: border-box;
+		background-clip: border-box;
+		box-sizing: border-box;
+	}
+</style>
